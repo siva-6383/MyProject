cbebae9cbaf6b402050e7c2178a660679e63949d
siva-6383 <115678109+siva-6383@users.noreply.github.com>
2022-10-20 17:03:12 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,16 @@
+package Teleapps;
+
+public class Example {
+	static void fun() throws IllegalAccessException {
+		System.out.println("Inside fun(). ");
+		throw new IllegalAccessException("demo");
+	}
+
+	public static void main(String args[]) {
+		try {
+			fun();
+		} catch (IllegalAccessException e) {
+			System.out.println("caught in ma" + "in.");
+		}
+	}
+}
